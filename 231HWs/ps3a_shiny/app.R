d973e330f0cb2936f1e3e8c5b48063a647c399f3
Willie Cohen <wcohen22@amherst.edu>
2021-03-07 22:24:33 -0500

    changed exercise b

--- 231HWs/ps3a_shiny/app.R
+++ 231HWs/ps3a_shiny/app.R
@@ -2,22 +2,35 @@
 library(shiny)
 
 ui <- fluidPage(
-  sliderInput(inputId = "num", 
+  navlistPanel(
+    tabPanel(
+      title = "Histogram",
+      sliderInput(inputId = "num", 
               label = "Choose a number", 
               value = 25, min = 1, max = 100),
-  textInput(inputId = "title",
+      textInput(inputId = "title",
             label = "Write a title",
             value = "Histogram of Random Normal Values"),
-  plotOutput("hist"),
-  verbatimTextOutput("stats")
+      plotOutput("hist")),
+    tabPanel(
+      title = "Summary of Data",
+      verbatimTextOutput("stats")
+    )
+    
 )
+    )
 
 server <- function(input, output) {
+  
+  data <- reactive({
+    rnorm(input$num)
+  })
+  
   output$hist <- renderPlot({
-    hist(rnorm(input$num), main = input$title)
+    hist(data(), main = input$title)
   })
   output$stats <- renderPrint({
-    summary(rnorm(input$num))
+    summary(data())
   })
 }
 
