7e23b11cf2c7a249e0b74da3a2799fd5d8ac8359
Willie Cohen <wcohen22@amherst.edu>
2021-03-07 22:10:38 -0500

    finished exercise b

---
+++ 231HWs/ps3a_shiny/app.R
@@ -0,0 +1,25 @@
+
+library(shiny)
+
+ui <- fluidPage(
+  sliderInput(inputId = "num", 
+              label = "Choose a number", 
+              value = 25, min = 1, max = 100),
+  textInput(inputId = "title",
+            label = "Write a title",
+            value = "Histogram of Random Normal Values"),
+  plotOutput("hist"),
+  verbatimTextOutput("stats")
+)
+
+server <- function(input, output) {
+  output$hist <- renderPlot({
+    hist(rnorm(input$num), main = input$title)
+  })
+  output$stats <- renderPrint({
+    summary(rnorm(input$num))
+  })
+}
+
+shinyApp(ui = ui, server = server)
+
